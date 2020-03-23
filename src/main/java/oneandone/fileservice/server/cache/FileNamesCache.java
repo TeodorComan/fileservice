64d9f09c781ea5553cf1b1c9ddf73188aef815ec
TeodorComan <TeodorComan@users.noreply.github.com>
2020-03-23 11:59:24 +0200

    Update FileNamesCache.java

--- src/main/java/oneandone/fileservice/server/cache/FileNamesCache.java
+++ src/main/java/oneandone/fileservice/server/cache/FileNamesCache.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.sun.xml.internal.fastinfoset.util.CharArray;
-
 import oneandone.fileservice.server.service.impl.NioFilesWrapper;
 
 @Component
